54db57c97ee658f8e469156b0fd8695bce237911
harisparavoor <harisparavoor@gmail.com>
2025-07-03 11:36:02 +0100

    some addon

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,48 @@
 var path = require('path');
-
+const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080';
 module.exports = {
     entry: './src/main/js/app.js',
     devtool: 'eval-source-map',
     cache: true,
-	mode: 'development',
+    mode: 'development',
     resolve: {
         alias: {
             'stompjs': __dirname + '/node_modules' + '/stompjs/lib/stomp.js',
         }
     },
     output: {
-        path: __dirname,
-        filename: './src/main/resources/static/built/bundle.js'
+        path: path.resolve(__dirname, 'dist'), // build output in 'dist' for development
+        filename: 'bundle.js',
+        publicPath: '/', // important for dev-server to serve from root
     },
-	module: {
-		rules: [
-			{
-				test: path.join(__dirname, '.'),
-				exclude: /(node_modules)/,
-				use: [{
-					loader: 'babel-loader',
-					options: {
-						presets: ["@babel/preset-env", "@babel/preset-react"]
-					}
-				}]
-			}
-		]
-	}
-	
-         };
+    module: {
+        rules: [
+            {
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }]
+            }
+        ]
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'), // serve static files if any
+        },
+        historyApiFallback: true, // useful for React Router
+        proxy: {
+            '/api': 'http://backendUrl:8080', // proxy API requests to backend
+            '/ws': {
+                target: 'http://backendUrl:8080',
+                ws: true
+            }
+        },
+        port: 3000,
+        hot: true,
+        open: true
+    }
+};
